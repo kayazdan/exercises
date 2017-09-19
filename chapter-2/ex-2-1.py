a8be99cef881a1b924a0bab15889d68060f096b8
Kaya Zdan <kaya-zdan@student.kirkwood.edu>
2017-09-19 13:17:42 +0000

    Address Program

--- chapter-2/ex-2-1.py
+++ chapter-2/ex-2-1.py
@@ -6,19 +6,19 @@
 # It will display the name and contact information for a teacher.
 
 # display the full name
-
+print ("Mr. Namey Name")
 
 # display the address
-
+print ("123 Driving dr.")
 
 # display the city, state and zip
-
+print ("Llanfairpwllgwyngyll, Wales 99999")
 
 # display the phone number
-
+print ("314-159-2653")
 
 # display the course area
-
+print ("CIS-121")
 
 
 
