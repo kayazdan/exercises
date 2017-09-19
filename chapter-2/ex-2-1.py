d1b5c851b152d6492df302394fdf027dfe40a65b
Kaya Zdan <kaya-zdan@student.kirkwood.edu>
2017-09-19 13:42:33 +0000

    Edited to add variables

--- chapter-2/ex-2-1.py
+++ chapter-2/ex-2-1.py
@@ -1,25 +1,21 @@
 # Programming Exercise 2-1
-# 
+#
 # Program to display one person's contact information.
 # This program needs no input
 # and performs no computations.
 # It will display the name and contact information for a teacher.
 
-# display the full name
-print ("Mr. Namey Name")
-
-# display the address
-print ("123 Driving dr.")
-
-# display the city, state and zip
-print ("Llanfairpwllgwyngyll, Wales 99999")
-
-# display the phone number
-print ("314-159-2653")
-
-# display the course area
-print ("CIS-121")
+name = "Mr. Namey Name"
+address = "123 Driving dr."
+city_state_zip = "Llanfairpwllgwyngyll, Wales 99999"
+phone_number = "314-159-2653"
+course = "CIS-121"
 
 
 
+print (name)
+print (address)
+print (city_state_zip)
+print (phone_number)
+print (course)
 
